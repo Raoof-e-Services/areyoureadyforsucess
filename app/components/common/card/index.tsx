a490e2d5c763cfea0a682ce1ef1fa289bbeb1756
Raoof-e-Services <raoofeservices@gmail.com>
2023-09-28 22:32:39 +0500

    structure and alignment is completed

--- app/components/common/card/index.tsx
+++ app/components/common/card/index.tsx
@@ -3,36 +3,46 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 interface CardData {
-  heading: string;
-  paragraph: string;
+  headingUrdu: string;
+  headingEnglish: string;
+  paragraphUrdu: string;
+  paragraphEnglish: string;
 }
 
 const cardData: CardData[] = [
   {
-    heading: 'Heading 1',
-    paragraph: 'This is the first paragraph.',
+    headingUrdu: 'Urdu Heading 1',
+    headingEnglish: 'English Heading 1',
+    paragraphUrdu: 'This is the first Urdu paragraph.This is the first Urdu paragraph.This is the first Urdu paragraph.This is the first Urdu paragraph.This is the first Urdu paragraph.This is the first Urdu paragraph.This is the first Urdu paragraph.This is the first Urdu paragraph.',
+    paragraphEnglish: 'This is the first English paragraph.',
   },
   {
-    heading: 'Heading 2',
-    paragraph: 'This is the second paragraph.',
+    headingUrdu: 'Urdu Heading 2',
+    headingEnglish: 'English Heading 2',
+    paragraphUrdu: 'This is the second Urdu paragraph.',
+    paragraphEnglish: 'This is the second English paragraph.',
   },
   // Add more data as needed
 ];
 
 const cardStyle: React.CSSProperties = {
-  width: '300px',
+  width: '80vw',
+  height:"70vh",
   border: '1px solid #ccc',
   boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
   padding: '20px',
   margin: '20px auto',
   backgroundColor: '#fff',
-  textAlign: 'center',
-  position: 'relative', // To allow heading animation outside the card
+ display:"flex",
+ justifyContent:"center",
+ alignItems:"center",
+ flexDirection:"column"
 };
 
 const paragraphStyle: React.CSSProperties = {
   fontSize: '16px',
   color: '#555',
+  width:"100%"
 };
 
 const buttonStyle: React.CSSProperties = {
@@ -51,6 +61,10 @@ const CardComponent: React.FC = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % cardData.length);
   };
 
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + cardData.length) % cardData.length);
+  };
+
   const currentCard = cardData[currentIndex];
 
   return (
@@ -58,7 +72,7 @@ const CardComponent: React.FC = () => {
       <AnimatePresence mode="wait">
         {/* Apply animation to the heading */}
         <motion.div
-          key={currentCard.heading} // Ensure a key change triggers animation
+          key={currentCard.headingEnglish} // Ensure a key change triggers animation
           initial={{ opacity: 0, y: '100vh' }} // Start from below the viewport
           animate={{ opacity: 1, y: 0 }} // Animate to full opacity and its default position
           exit={{ opacity: 0, y: '-100vh' }} // Exit animation (heading moves above viewport)
@@ -67,20 +81,37 @@ const CardComponent: React.FC = () => {
             fontSize: '24px',
             color: '#333',
             marginBottom: '10px',
-            position: 'absolute', // Position absolutely within the card
-            left: '50%', // Center horizontally
             transform: 'translateX(-50%)', // Center horizontally
             width: '100%', // Take full width of the card
+            
           }}
         >
-          {currentCard.heading}
+          <div style={{textAlign:"left"}}>
+          {currentCard.headingEnglish}
+          </div>
+          <div style={{textAlign:"right"}}>
+          {currentCard.headingUrdu}
+          </div>
         </motion.div>
       </AnimatePresence>
       {/* The paragraph and buttons remain static */}
-      <div style={paragraphStyle}>{currentCard.paragraph}</div>
+      
+      <div style={{...paragraphStyle,textAlign:"left"}}>
+        
+        {currentCard.paragraphUrdu}
+        
+      </div>
+      <div style={{...paragraphStyle,textAlign:"right"}}>
+        {currentCard.paragraphEnglish}
+        
+      </div>
+      
       <div>
+        <button style={buttonStyle} onClick={handlePrevClick}>
+          Previous
+        </button>
         <button style={buttonStyle} onClick={handleNextClick}>
-          Next
+            Next
         </button>
       </div>
     </div>
